7745ac309174317ae2d6a5f291ca8ab82893279c
Manish Kumar <manish15052000@gmail.com>
2020-10-12 23:44:33 +0530

    Updates

---
+++ precache-manifest.14ab4ce2350289b04344ad9f845d43f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46052b3e75a3f84cf941dfcc6e53b6d",
+    "url": "/meme-generator-reactJS/index.html"
+  },
+  {
+    "revision": "bafff304ea561116907c",
+    "url": "/meme-generator-reactJS/static/css/main.f9d87bf0.chunk.css"
+  },
+  {
+    "revision": "125f56e79ad11c06aaf5",
+    "url": "/meme-generator-reactJS/static/js/2.06eaaf96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator-reactJS/static/js/2.06eaaf96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bafff304ea561116907c",
+    "url": "/meme-generator-reactJS/static/js/main.afdebeaf.chunk.js"
+  },
+  {
+    "revision": "a7a32fe79a753c7dae06",
+    "url": "/meme-generator-reactJS/static/js/runtime-main.4fb652c5.js"
+  },
+  {
+    "revision": "e84a0a5ca72c5c51c95112ad96ae3dea",
+    "url": "/meme-generator-reactJS/static/media/bg.e84a0a5c.svg"
+  }
+]);
